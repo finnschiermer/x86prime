b0ea5de376f9a2ccdf3493d077dfc99b9e3f3536
Finn Schiermer <fsa@realm.io>
2018-09-30 20:31:55 +0200

    bugfix: improved analysis of stack-pointer movements

--- stack.ml
+++ stack.ml
@@ -3,39 +3,48 @@
 (* Should also handle direct adjustment of %rsp *)
 (* and memory addressing relative to %rsp *)
 
-let rec stack_change lines change =
+let rec stack_change lines pushes pops =
   let open Ast in
   match lines with
-  | Ok(PuPo(PUSH,_)) :: others -> stack_change others (change - 8)
-  | Ok(PuPo(POP,_)) :: others -> stack_change others (change + 8)
-  | Ok(Alu2(_)) :: others | Ok(Move2(_)) :: others | Ok(Ignored(_)) :: others -> stack_change others change
-  | _ :: _ -> change
-  | [] -> change
+  | Ok(PuPo(PUSH,_)) :: others -> stack_change others (pushes + 1) pops
+  | Ok(PuPo(POP,_)) :: others -> stack_change others pushes (pops + 1)
+  | Ok(Alu2(_)) :: others | Ok(Move2(_)) :: others | Ok(Ignored(_)) :: others -> stack_change others pushes pops
+  | _ :: _ -> pushes,pops
+  | [] -> pushes,pops
+
+exception PushAndPop
 
 let rec process_all_blocks lines =
-  let adjustment = stack_change lines 0 in
-  if adjustment = 0 then rewrite_block lines 0 None
-  else if adjustment < 0 then process_push_block lines adjustment
-  else  process_pop_block lines adjustment
+  let (pushes,pops) = stack_change lines 0 0 in
+  if pushes = 0 && pops = 0 then rewrite_block lines 0 None
+  else if pushes <> 0 && pops = 0 then process_push_block lines pushes
+  else if pops <> 0 && pushes = 0 then process_pop_block lines pops
+  else process_push_pop_block lines pushes pops
 
-and process_pop_block block adjustment =
-  let sp_adjust = Ok(Ast.Alu2(ADD,Ast.Imm(Printf.sprintf "%d" adjustment),Ast.Reg("%rsp"))) in
+and process_pop_block block pops =
+  let sp_adjust = Ok(Ast.Alu2(ADD,Ast.Imm(Printf.sprintf "%d" (8 * pops)),Ast.Reg("%rsp"))) in
   rewrite_block block 0 (Some sp_adjust)
 
-and process_push_block block adjustment =
-  let sp_adjust = Ok(Ast.Alu2(ADD,Ast.Imm(Printf.sprintf "%d" adjustment),Ast.Reg("%rsp"))) in
-  sp_adjust :: (rewrite_block block (0 - 8 - adjustment) None)
+and process_push_block block pushes =
+  let sp_adjust = Ok(Ast.Alu2(SUB,Ast.Imm(Printf.sprintf "%d" (8 * pushes)),Ast.Reg("%rsp"))) in
+  sp_adjust :: (rewrite_block block (8 * pushes) None)
+
+and process_push_pop_block block pushes pops =
+  let sp_adjust_before = Ok(Ast.Alu2(SUB,Ast.Imm(Printf.sprintf "%d" (8 * pushes)),Ast.Reg("%rsp"))) in
+  let sp_adjust_after = Ok(Ast.Alu2(ADD,Ast.Imm(Printf.sprintf "%d" (8 * pops)),Ast.Reg("%rsp"))) in
+  sp_adjust_before :: rewrite_block block (8 * pushes) (Some sp_adjust_after)
 
 and rewrite_block block curr_sp terminator =
   let open Ast in
   match block with
   | Ok(PuPo(PUSH,reg)) :: rest -> 
+     let curr_sp = curr_sp - 8 in
      if curr_sp = 0 then
-       Ok(Move2(MOV,reg,EaS("%rsp"))) :: rewrite_block rest (curr_sp - 8) terminator
+       Ok(Move2(MOV,reg,EaS("%rsp"))) :: rewrite_block rest curr_sp terminator
      else
-       Ok(Move2(MOV,reg,EaDS((Printf.sprintf "%d" (curr_sp)), "%rsp"))) :: rewrite_block rest (curr_sp - 8) terminator
+       Ok(Move2(MOV,reg,EaDS((Printf.sprintf "%d" (curr_sp)), "%rsp"))) :: rewrite_block rest curr_sp terminator
   | Ok(PuPo(POP,reg)) :: rest ->
-     if curr_sp == 0 then
+     if curr_sp = 0 then
        Ok(Move2(MOV,EaS("%rsp"),reg)) :: rewrite_block rest (curr_sp + 8) terminator
      else
        Ok(Move2(MOV,EaDS((Printf.sprintf "%d" (curr_sp)), "%rsp"),reg)) :: rewrite_block rest (curr_sp + 8) terminator

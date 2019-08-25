c6b6208bbd1a7f9b70cebe5b7a7b7aa10dece0cb
Finn Schiermer <fsa@realm.io>
2019-08-25 16:19:56 +0200

    support unaligned memory accesses

--- memory.ml
+++ memory.ml
@@ -1,18 +1,16 @@
 exception OutOfSimulatedMemory
 exception Argghhh
 
-type t = (Int64.t, (Int64.t array)) Hashtbl.t
+type t = (int, bytes) Hashtbl.t
 
 let create () : t = Hashtbl.create 16
 
 let txl_addr (addr : Int64.t) =
-  let word_addr = Int64.shift_right_logical addr 3 in
-  let byte_offset = Int64.sub addr (Int64.shift_left word_addr 3) in
-  let page_addr = Int64.shift_right_logical word_addr 8 in
-  let page_offset = Int64.sub word_addr (Int64.shift_left page_addr 8) in
-  (page_addr, page_offset, byte_offset)
+  let page_addr = Int64.shift_right_logical addr 10 in
+  let byte_offset = Int64.sub addr (Int64.shift_left page_addr 10) in
+  (Int64.to_int page_addr, Int64.to_int byte_offset)
 
-type page = Int64.t array
+type page = bytes
 
 let get_page mem page_addr =
   match Hashtbl.find_opt mem page_addr with
@@ -20,45 +18,44 @@ let get_page mem page_addr =
   | None -> if (Hashtbl.length mem > 10000) then
               raise OutOfSimulatedMemory
             else
-              let page = (Array.make 256 Int64.zero) in
+              let page = (Bytes.make 1024 (Char.chr 0)) in
               Hashtbl.add mem page_addr page;
               page
 
-let ext_quad page offset = page.(Int64.to_int offset)
-
-let ins_quad page offset quad = page.(Int64.to_int offset) <- quad
-
-let ins_byte offset quad value =
-  let mask = Int64.lognot (Int64.shift_left (Int64.of_int 0x0FF) (8 * Int64.to_int offset)) in
-  let aligned_value = Int64.shift_left (Int64.of_int value) (8 * Int64.to_int offset) in
-  let new_value = Int64.logor aligned_value (Int64.logand quad mask) in
-  new_value
-
-let ext_byte offset quad =
-  let aligned = Int64.shift_right_logical quad (8 * Int64.to_int offset) in
-  Int64.to_int (Int64.logand aligned (Int64.of_int 0x0FF))
-
 let read_byte mem addr =
-  let page_addr, quad_offset, byte_offset = txl_addr addr in
+  let page_addr, byte_offset = txl_addr addr in
   let page = get_page mem page_addr in
-  let quad = ext_quad page quad_offset in
-  let byte = ext_byte byte_offset quad in
-  byte
+  Char.code (Bytes.get page byte_offset)
 
 let write_byte mem addr value =
-  let page_addr, quad_offset, byte_offset = txl_addr addr in
+  let page_addr, byte_offset = txl_addr addr in
   let page = get_page mem page_addr in
-  let quad = ext_quad page quad_offset in
-  let new_quad = ins_byte byte_offset quad value in
-  ins_quad page quad_offset new_quad
+  Bytes.set page byte_offset (Char.chr (value land 0xff))
+
 
 let write_quad mem addr value =
-  let page_addr, quad_offset, byte_offset = txl_addr addr in
-  let page = get_page mem page_addr in
-  ins_quad page quad_offset value
+  write_byte mem addr (Int64.to_int (Int64.shift_right_logical value 56));
+  write_byte mem (Int64.add addr (Int64.of_int 1)) (Int64.to_int (Int64.shift_right_logical value 48));
+  write_byte mem (Int64.add addr (Int64.of_int 2)) (Int64.to_int (Int64.shift_right_logical value 40));
+  write_byte mem (Int64.add addr (Int64.of_int 3)) (Int64.to_int (Int64.shift_right_logical value 32));
+  write_byte mem (Int64.add addr (Int64.of_int 4)) (Int64.to_int (Int64.shift_right_logical value 24));
+  write_byte mem (Int64.add addr (Int64.of_int 5)) (Int64.to_int (Int64.shift_right_logical value 16));
+  write_byte mem (Int64.add addr (Int64.of_int 6)) (Int64.to_int (Int64.shift_right_logical value 8));
+  write_byte mem (Int64.add addr (Int64.of_int 7)) (Int64.to_int value)
 
 let read_quad mem addr =
-  let page_addr, quad_offset, byte_offset = txl_addr addr in
-  let page = get_page mem page_addr in
-  let quad = ext_quad page quad_offset in
-  quad
+  let v7 = Int64.of_int (read_byte mem addr) in
+  let v6 = Int64.of_int (read_byte mem (Int64.add addr (Int64.of_int 1))) in
+  let v5 = Int64.of_int (read_byte mem (Int64.add addr (Int64.of_int 2))) in
+  let v4 = Int64.of_int (read_byte mem (Int64.add addr (Int64.of_int 3))) in
+  let v3 = Int64.of_int (read_byte mem (Int64.add addr (Int64.of_int 4))) in
+  let v2 = Int64.of_int (read_byte mem (Int64.add addr (Int64.of_int 5))) in
+  let v1 = Int64.of_int (read_byte mem (Int64.add addr (Int64.of_int 6))) in
+  let v0 = Int64.of_int (read_byte mem (Int64.add addr (Int64.of_int 7))) in
+  Int64.logor
+    (Int64.logor
+      (Int64.logor (Int64.shift_left v7 56) (Int64.shift_left v6 48))
+      (Int64.logor (Int64.shift_left v5 40) (Int64.shift_left v4 32)))
+    (Int64.logor
+      (Int64.logor (Int64.shift_left v3 24) (Int64.shift_left v2 16))
+      (Int64.logor (Int64.shift_left v1 8) v0))

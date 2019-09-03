af7720e051ab8a676909f12d9d60d5efb22cfc58
Finn Schiermer <fsa@realm.io>
2019-09-03 20:17:16 +0200

    place links to executable in bin/ dir

--- buildall.sh
+++ buildall.sh
@@ -2,7 +2,10 @@ ocamlbuild -use-menhir primify.native
 ocamlbuild -use-menhir prasm.native
 ocamlbuild -use-menhir prun.native
 ocamlbuild -use-menhir prerf.native
-ln -fs primify.native primify
-ln -fs prasm.native prasm
-ln -fs prun.native prun
-ln -fs prerf.native prerf
+mkdir -p bin
+pushd bin
+ln -fs ../primify.native primify
+ln -fs ../prasm.native prasm
+ln -fs ../prun.native prun
+ln -fs ../prerf.native prerf
+popd
